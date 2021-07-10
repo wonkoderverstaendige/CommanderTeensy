04dae15b0a5662746c878492af02a1aa7eb67b0c
Ronny Eichler <r.eichler@science.ru.nl>
2021-07-10 11:15:54 +0200

    Tidying up visualisation.js

--- web/visualisation.js
+++ web/visualisation.js
@@ -9,20 +9,18 @@ function documentReady(fn) {
 }
 
 function add_label(grid, channel_name, color, box=null, button=null) {
-    div = document.createElement("div");
+    const div = document.createElement("div");
     div.className = "label";
     div.id = channel_name;
-    div.style = "background-color: " + color;
-    var p = document.createElement("div");
+    div.style.backgroundColor = color;
+    const p = document.createElement("div");
     p.className = "channel_name";
     p.id = channel_name;
     p.textContent = channel_name;
-    // tn = document.createTextNode(channel_name);
 
     if (box != null) {
         div.appendChild(box);
     }
-    // div.appendChild(tn);
     div.appendChild(p);
     if (button != null) {
           div.appendChild(button);
@@ -31,79 +29,78 @@ function add_label(grid, channel_name, color, box=null, button=null) {
     return div;
 }
 
-
 documentReady(function() {
-    ws = start_websocket(5678);
-    graphs = new GraphCollection();
+    const ws = start_websocket(5678);
+    const graphs = new GraphCollection();
 
-    graph_analog = new Graph('canvas_analog');
+    let graph_analog = new Graph('canvas_analog');
     graph_analog.msPerPixel = 5;
 
-    graph_states = new Graph('canvas_states');
+    let graph_states = new Graph('canvas_states');
     graph_states.msPerPixel = 5;
 
-    graph_digital = new Graph('canvas_digital');
+    let graph_digital = new Graph('canvas_digital');
     graph_digital.msPerPixel = 5;
 
-    var unit_fields_analog = [];
-    var unit_names_analog = ['V', 'V', 'V', 'V', 'V', 'V', 'V', 'V'];
+    let unit_fields_analog = [];
+    const unit_names_analog = ['V', 'V', 'V', 'V', 'V', 'V', 'V', 'V'];
     for (let i=0; i<8; i++) {
-        lbl = document.createElement('div');
+        const lbl = document.createElement('div');
         lbl.className = 'unit_field analog';
-        lbl.id = "analog_" + i.toString();
+        lbl.id = `analog_${i}`;
         unit_fields_analog.push(lbl);
     }
 
-    var unit_fields_states = [];
-    var unit_names_states = ['cm', 'cm/s', 'cm/s²', 'Δlux', '°C', 'mA', 'Hz', 'kW'];
+    let unit_fields_states = [];
+    const unit_names_states = ['cm', 'cm/s', 'cm/s²', 'Δlux', '°C', 'mA', 'Hz', 'kW'];
     for (let i=0; i<8; i++) {
-        lbl = document.createElement('div');
+        const lbl = document.createElement('div');
         lbl.className = 'unit_field states';
-        lbl.id = "states_" + i.toString();
+        lbl.id = `states_${i}`;
         unit_fields_states.push(lbl);
     }
 
-    var indicators = [];
-    for (i=0; i<24; i++) {
-        ind = document.createElement('div');
+    let indicators = [];
+    for (let i=0; i<24; i++) {
+        const ind = document.createElement('div');
         ind.className = "indicator";
-        ind.id = "digital"+i.toString();
+        ind.id = `digital_${i}`;
         indicators.push(ind);
     }
 
-    var buttons = [];
-    for (i=0; i<8; i++) {
-        btn = document.createElement('button');
-        var text = document.createTextNode('toggle');
+    let buttons = [];
+    for (let i=0; i<8; i++) {
+        const btn = document.createElement('button');
+        const text = document.createTextNode('toggle');
         btn.appendChild(text)
         btn.className = "toggle_button";
-        btn.id = "digital"+(16+i).toString();
+        btn.id = `digital_${16+i}`;
         btn.onclick = btn_clicked;
         buttons.push(btn);
     }
 
     // analog graph
-    var label_grid = document.querySelector('#analog_grid');
-    var num_lines = 8;
+    let label_grid = document.querySelector('#analog_grid');
+    let num_lines = 8;
     for (let i = 0; i < num_lines; i++) {
         const line = graph_analog.add_line();
         line.scaleFactor = 1/2**16;
         line.offset = 0;
         line.color = 'hsl(' + (180/num_lines*i).toString() + ', 80%, 60%)';
-        add_label(label_grid, 'analog_input_' + i.toString(), line.color, button=unit_fields_analog[i]);
+        add_label(label_grid, `analog_input_${i}`, line.color, unit_fields_analog[i]);
     }
 
     // states graph
     label_grid = document.querySelector('#states_grid');
     num_lines = 8;
-    var scale_factors = [1/2**16.5, 1/2**10, 1/2**12, 1/2**12,
+    const scale_factors = [1/2**16.5, 1/2**10, 1/2**12, 1/2**12,
                         1/25, 1/2**22, 1/2**22, 1/2**22];
     for (let i = 0; i < num_lines; i++) {
         const line = graph_states.add_line();
         line.scaleFactor = scale_factors[i]/4;  // should be 32 for full range, just for ease of visualisation
         line.offset = 1 - 1/num_lines*i - line.scaleFactor; //0.5;
         line.color = 'hsl(' + (180/num_lines*i+180).toString() + ', 40%, 50%)';
-        add_label(label_grid, 'states_input_' + i.toString(), line.color, button=unit_fields_states[i]);
+        add_label(label_grid, `states_input_${i}`, line.color, unit_fields_states[i]);
     }
 
     //digital graph
@@ -120,9 +117,8 @@ documentReady(function() {
         } else {
             line.color = 'hsl(' + (120/num_lines*i+120).toString() + ', 80%, 60%)';
         }
-
-        var btn = (i < 16) ? null : buttons[i-16];
-        add_label(label_grid, "digital_" + data_type + "_" + i.toString(), line.color,  box=indicators[i], button=btn);
+        const btn = (i < 16) ? null : buttons[i-16];
+        add_label(label_grid, `digital_${data_type}_${i}`, line.color,  indicators[i], btn);
     }
 
 
@@ -140,48 +136,42 @@ documentReady(function() {
     }   
 
     ws.onmessage = function(event) {
-        if (typeof event.data === 'string') {
-            try {
-                var packet = JSON.parse(event.data);
-            } catch (e) {
-                console.log(e);
-                console.log(event.data);
-                return;
-            }
-            graph_analog.appendPacket(packet.us_start, packet.analog);
-            graph_states.appendPacket(packet.us_start, packet.states);
-            // console.log(packet.states);
-            graph_digital.appendPacket(packet.us_start, packet.digitalIn.concat(packet.digitalOut));
+        if (typeof event.data !== 'string') return;
 
-            var lbl;
-            // analog charts values
-            for (let i=0; i<8; i++) {
-                lbl = document.querySelector('.unit_field#analog_' + i.toString());
-                lbl.textContent = (packet.analog[i] / 2**16 * 3.3).toFixed(2) + ' ' + unit_names_analog[i];
-            }
+        let packet;
+        try {
+            packet = JSON.parse(event.data);
+        } catch (e) {
+            console.error(e);
+            console.log(event.data);
+            return;
+        }
+        graph_analog.appendPacket(packet.us_start, packet.analog);
+        graph_states.appendPacket(packet.us_start, packet.states);
+        graph_digital.appendPacket(packet.us_start, packet.digitalIn.concat(packet.digitalOut));
 
-            // state charts values
-            for (let i=0; i<8; i++) {
-                lbl = document.querySelector('.unit_field#states_' + i.toString());
-                lbl.textContent = (packet.states[i] >= 0 ? '0' : '') + (packet.states[i] / 100).toFixed(2) + ' ' + unit_names_states[i];
-            }
+        let lbl;
+        // analog charts values
+        for (let i=0; i<8; i++) {
+            lbl = document.querySelector(`.unit_field#analog_${i}`);
+            lbl.textContent = (packet.analog[i] / 2**16 * 3.3).toFixed(2) + ' ' + unit_names_analog[i];
+        }
 
-            // digital charts indicators
-            for (let i=0; i<24; i+=1) {
-                if (i<16) {
-                    if (packet.digitalIn[i]) {
-                            indicators[i].style = 'background-color: #0F0';
-                    } else {
-                            indicators[i].style = 'background-color: #000';
-                    }
-                } else {
-                    if (packet.digitalOut[i-16]) {
-                            indicators[i].style = 'background-color: #F00';
-                    } else {
-                            indicators[i].style = 'background-color: #000';
-                    }
-                }
+        // state charts values
+        for (let i=0; i<8; i++) {
+            lbl = document.querySelector(`.unit_field#states_${i}`);
+            lbl.textContent = (packet.states[i] >= 0 ? '0' : '') + (packet.states[i] / 100).toFixed(2) + ' ' + unit_names_states[i];
+        }
+
+        // digital charts indicators
+        for (let i=0; i<24; i+=1) {
+            let bg_color;
+            if (i<16) {
+                bg_color = packet.digitalIn[i] ? '#0F0' : '#000';
+            } else {
+                bg_color = packet.digitalOut[i-16] ? '#F00' : '#000';
             }
+            indicators[i].style.backgroundColor = bg_color;
         }
     }
 });
