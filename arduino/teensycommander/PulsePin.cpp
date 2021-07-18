37c2c0e81dfe1fc6db1ec96e9b9fa9769699358c
Ronny Eichler <wonkoderverstaendige@gmail.com>
2021-07-18 19:35:20 +0200

    Add missing files...

---
+++ arduino/teensycommander/PulsePin.cpp
@@ -0,0 +1,41 @@
+#include "PulsePin.h"
+PulsePin::PulsePin(int pinID, int pinNr, int polarity) {
+  this->pinID = pinID;
+  this->pinNr = pinNr;
+  
+  // set polarity of ACTIVE state
+  this->polarity = polarity;
+  pinMode(pinID, OUTPUT);
+  
+  this->nextChangeTime = 0;
+  this->run = true;
+}
+
+void PulsePin::update() {
+  if (run) {
+    if(current_micros < this->nextChangeTime) {
+      digitalWriteFast(this->pinNr, this->polarity);
+    } else {
+      digitalWriteFast(this->pinNr, !this->polarity);
+    }
+  }
+}
+
+void PulsePin::pulse(unsigned long duration) {
+  this->nextChangeTime = current_micros + duration;
+  Serial.println(this->nextChangeTime);
+}
+
+void PulsePin::restart() {
+  this->run = true;
+  this->nextChangeTime = 0;
+  digitalWriteFast(this->pinNr, !this->polarity);
+}
+
+void PulsePin::stop() {
+	this->run = false;
+}
+
+int PulsePin::getId() {
+  return this->pinID;
+}
---
+++ arduino/teensycommander/PulsePin.h
@@ -0,0 +1,32 @@
+#ifndef PULSDPIN_H
+#define PULSDPIN_H
+#include <Arduino.h>
+
+extern elapsedMicros current_micros;
+
+class PulsePin {
+  
+  private:
+    int pinID;
+  	int pinNr;
+  	int polarity;
+  	unsigned long nextChangeTime;
+  	boolean run;
+    
+  public:
+    // Setup pin 
+    PulsePin(int pinID, int pinNr, int polarity);
+    
+    // Check the states
+    void update();
+	
+  	// Stop updating etc.
+  	void stop();
+  	
+  	void restart();
+   
+    void pulse(unsigned long duration);
+
+    int getId();
+};
+#endif
