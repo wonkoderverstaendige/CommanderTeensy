034a646ca13c472e46c87aee9f14cceecca814bb
Ronny Eichler <wonkoderverstaendige@gmail.com>
2021-08-07 13:00:48 +0200

    Improve reliability and immediacy of toggle buttons in web interface

--- web/webgl_plotting.js
+++ web/webgl_plotting.js
@@ -153,7 +153,7 @@ function createUI() {
                     button.appendChild(text)
                     button.className = "toggle_button";
                     button.id = `${partitionID}_${i}`;
-                    button.onclick = btn_clicked;
+                    button.onmousedown = btn_clicked;
                 }
 
                 add_label(label_grid, `${partitionID}_${i}`, color, box, button);
@@ -243,39 +243,4 @@ function btn_clicked(event) {
     if (ws) {
         ws.send(event.target.id);
     }
-}
-
-/**
- * Assumes h, s, and l are contained in the set [0, 1] and
- * returns r, g, and b in the set [0, 1].
- * FROM: https://stackoverflow.com/a/9493060
- *
- * @param   {number}  h       The hue
- * @param   {number}  s       The saturation
- * @param   {number}  l       The lightness
- * @return  {Array}           The RGB representation
- */
-function hslToRgb(h, s, l) {
-    let r, g, b;
-
-    if (s === 0) {
-        r = g = b = l; // achromatic
-    } else {
-        let hue2rgb = function hue2rgb(p, q, t) {
-            if (t < 0) t += 1;
-            if (t > 1) t -= 1;
-            if (t < 1 / 6) return p + (q - p) * 6 * t;
-            if (t < 1 / 2) return q;
-            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-            return p;
-        }
-
-        let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        let p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1 / 3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1 / 3);
-    }
-
-    return [r, g, b];
 }
